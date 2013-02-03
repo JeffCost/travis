3e3fffc9201c4028ed409e758b3bd19ffad25e38
JeffCost <jefersonc@gmail.com>
2013-02-02 22:09:27 -0600

    Adds database seed action for all modules

--- tasks/test.php
+++ tasks/test.php
@@ -8,9 +8,7 @@ class Travis_Test_Task {
     }
 
     public function installapp($arguments)
-    {
-        echo "Installing application...";
-        
+    {   
         $files = glob("./bundles/*");
 
         $string = "\n".'<?php' ."\n".'return array(';
@@ -32,38 +30,39 @@ class Travis_Test_Task {
 
         require path('sys').'cli/dependencies.php';
 
+        echo "Installing migrations table...\n";
         \Laravel\CLI\Command::run(array('migrate:install'));
-
+        echo "Runnign migrations...\n";
         \Laravel\CLI\Command::run(array('migrate'));
 
-
-        // //run all tasks
-        // $files = glob('../mwi/bundles/' . "*");
-        // foreach($files as $file)
-        // {
-        //     if(is_dir($file))
-        //     {
-        //         $schema_path = '../mwi/bundles/'.basename($file).'/tasks/schema.php'; 
+        //run all tasks
+        $files = glob("./bundles/*");
+        foreach($files as $file)
+        {
+            if(is_dir($file))
+            {
+                $schema_path = './bundles/'.basename($file).'/tasks/schema.php'; 
                     
-        //         if(\File::exists($schema_path))
-        //         {
-        //             $action = 'install';
-        //             include_once $schema_path;
-        //             // Does the class exists?
-        //             $class = basename($file).'_Schema_Task';
-        //             if(class_exists($class))
-        //             {
-        //                 $schema_class = new $class;
-        //                 // The action is callable?
-        //                 if(is_callable(array($schema_class, $action)))
-        //                 {
-        //                     $schema_class->$action();
-                            
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-    }
+                if(\File::exists($schema_path))
+                {
+                    $action = 'install';
+                    include_once $schema_path;
+                    // Does the class exists?
+                    $class = basename($file).'_Schema_Task';
+                    if(class_exists($class))
+                    {
+                        $schema_class = new $class;
+                        // The action is callable?
+                        if(is_callable(array($schema_class, $action)))
+                        {
+                            echo "Seeding database for [".basename($file)."]\n"
+                            $schema_class->$action();
+                        }
+                    }
+                }
+            }
+        }
 
+        echo "Application is ready for tests...\n";
+    }
 }
