357a85d46a9c526c72a99b4ec9b5d2a6156cec16
JeffCost <jefersonc@gmail.com>
2013-02-03 00:59:15 -0600

    Updates test file

--- tasks/test.php
+++ tasks/test.php
@@ -9,6 +9,8 @@ class Travis_Test_Task {
 
     public function installapp($arguments)
     {   
+        \Session::load();
+        
         $files = glob("./bundles/*");
 
         $string = "\n".'<?php' ."\n".'return array(';
@@ -17,8 +19,6 @@ class Travis_Test_Task {
         {
             if(is_dir($file))
             {
-                Bundle::register(basename($file));
-                Bundle::start(basename($file));
                 $modules_list[] = basename($file);
                 echo "Found module [".basename($file)."]\n";
                 $string .= '\''.basename($file).'\' => array('."\n";
@@ -34,10 +34,6 @@ class Travis_Test_Task {
         fclose($new_bundles_file);
         echo "Bundles file updated successfully.\n";
 
-        $names = Bundle::names();
-        var_dump($names);
-        exit(0);
-
         require path('sys').'cli/dependencies.php';
 
         echo "Installing migrations table...\n";
@@ -49,10 +45,36 @@ class Travis_Test_Task {
 
         foreach ($modules_list as $module) 
         {
+            Bundle::register($module);
             echo "\nRunning migration for [".$module."]\n";
             \Laravel\CLI\Command::run(array('migrate', $module));
+            Bundle::disable($module);
         }
         echo "\n";
+
+        Bundle::register('modules');
+        Bundle::start('modules');
+        foreach ($modules_list as $module => $module_path)
+        {
+            $mod = \Modules\Module::make($module)->is_valid();
+
+            $new_bundle = new \Modules\Model\Module;
+            $new_bundle->name        = $mod->name;
+            $new_bundle->slug        = $mod->slug;
+            $new_bundle->description = isset($mod->description) ? $mod->description : '';
+            $new_bundle->version     = $mod->version ;
+            $new_bundle->is_frontend = isset($mod->is_frontend) ? $mod->is_frontend : 0;
+            $new_bundle->is_backend  = isset($mod->is_backend) ? $mod->is_backend : 0;
+            $new_bundle->is_core     = isset($mod->is_core) ? 1 : 0;;
+            $new_bundle->required    = $mod->decode('required');
+            $new_bundle->recommended = $mod->decode('recommended');
+            $new_bundle->options     = $mod->decode('options'); 
+            $new_bundle->roles       = $mod->decode('roles');
+            $new_bundle->menu        = $mod->decode('menu');
+            $new_bundle->enabled     = 1;
+            $new_bundle->save();
+        }
+        Bundle::disable('modules');
 
         //run all tasks
         $files = glob("./bundles/*");
