c3f607413af354565ec9c0c40d70edda901e2cff
JeffCost <jefersonc@gmail.com>
2013-02-02 20:33:48 -0600

    Update tests task

---
+++ tasks/mwitests.php
@@ -0,0 +1,15 @@
+<?php
+
+class Mwitests_Task {
+
+    public function run($arguments)
+    {
+        // echo help
+    }
+
+    public function installapp($arguments)
+    {
+        echo "Installing application...";
+    }
+
+}
